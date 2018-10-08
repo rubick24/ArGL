8729b69fef467f5e3f6f826c82f8699092e2c40c
申鹏飞 <spf@arthas.me>
2018-10-08 10:13:04 +0800

    keyword

--- dist/argl.d.ts
+++ dist/argl.d.ts
@@ -2,6 +2,7 @@
 // Project: ArGL
 // Definitions by: shen pengfei <https://github.com/deadalusmask>
 
+import { vec2, vec3, vec4, mat2, mat3, mat4, quat } from 'gl-matrix'
 
 export as namespace ArGL;
 
@@ -20,7 +21,7 @@ export class ArGL {
   el: HTMLDivElement
   canvas: HTMLCanvasElement
   loadingBar: HTMLProgressElement
-  resource: Resource
+  resource: any
   gl: WebGL2RenderingContext
   textures: WebGLTexture[]
 
@@ -42,7 +43,7 @@ export class ArGL {
   /**
    * start render circle
    */
-  async start(): void
+  start(): void
 
   /**
    * bind images to this.resource.images
@@ -53,6 +54,130 @@ export class ArGL {
   /**
    * create textures from this.resource.images
    */
-  async loadTexture(): WebGLTexture[]
+  loadTexture(): WebGLTexture[]
 
+}
+
+export class Shader {
+  gl: WebGL2RenderingContext
+  program: WebGLProgram
+
+  constructor(gl: WebGL2RenderingContext, vsSource: string, fsSource: string)
+
+  /**
+   * active this shader to draw
+   */
+  use(): void
+
+  /**
+   * set bool uniform
+   * @param name
+   * @param value
+   */
+  setBool(name: string, value: boolean): void
+
+  /**
+   * set int uniform
+   * @param name
+   * @param value
+   */
+  setInt(name: string, value: number): void
+
+  /**
+   * set float uniform
+   * @param name
+   * @param value
+   */
+  setFloat(name: string, value: number)
+  setVec2(name: string, vec2: vec2)
+  setVec3(name: string, vec3: vec3)
+  setVec4(name: string, vec4: vec4)
+  setMat2(name: string, mat2: mat2)
+  setMat3(name: string, mat3: mat3)
+  setMat4(name: string, mat4: mat4)
+}
+
+export class Camera {
+  position: vec3
+  orientation: quat
+  zoom: number
+
+  constructor(position?: vec3, orientation?: quat, zoom?: number)
+
+  /**
+   * get viewMatrix of this camera
+   * @return mat4 viewMatrix
+   */
+  getViewMatrix(): mat4
+
+  /**
+   * translate camera by v
+   * @param v
+   */
+  translate(v: vec3): void
+
+  /**
+   * rotate camera with given axis and angle
+   * @param angle
+   * @param axis
+   */
+  rotate(angle: number, axis: vec3): void
+
+  yaw(angle: number): void
+  pitch(angle: number): void
+  roll(angle: number): void
+
+  /**
+   * zoom in/out the camera
+   * @param yoffset
+   */
+  processZoom(yoffset: number): void
+}
+
+export class FreeMoveCamera extends Camera {
+
+  static Movement
+
+  /**
+   * move camera with given direction and given steps
+   * @param direction
+   * @param step
+   */
+  processMove(direction: vec3, step: number): void
+
+  /**
+   * add desktop free move control
+   * @param currentlyPressedKeys
+   * @param step
+   * @param mouseInput
+   * @param mouseSensitivity
+   * @param keys ['w', 's', 'a', 'd', ' ', 'Shift', 'q', 'e']
+   */
+  desktopFreeMoveControl(currentlyPressedKeys: Map<string, boolean>, step: number, mouseInput: any, mouseSensitivity: number, keys: string[]): void
+
+
+}
+
+export class OrbitCamera extends Camera {
+
+  /**
+   * orbit camera will always look at center and rotate by the center
+   * @param position
+   * @param center
+   * @param zoom
+   */
+  constructor(position?: vec3, center?: vec3, zoom?: number)
+
+  /**
+   * rotate x radian horizontal then y radian vertical
+   * @param radianX
+   * @param radianY
+   */
+  processRotate(radianX: number, radianY: number)
+
+  /**
+   * add orbitControl to argl
+   * @param argl
+   */
+  orbitControl(argl: ArGL)
 }
