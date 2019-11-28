5f91541de1ad0b973059bc1fede6eea0bd245c63
拉比克 <shenpengfei@bilibili.com>
2019-11-28 19:32:08 +0800

    stash

--- src/gltf-loader/getAnimations.ts
+++ src/gltf-loader/getAnimations.ts
@@ -2,8 +2,15 @@ import { IAccessor } from './interfaces'
 import { GlTF, Animation, AnimationChannel } from '../types/glTF'
 
 
-export default (josn: GlTF, accessors: IAccessor[]) => {
+export default (json: GlTF, accessors: IAccessor[]) => {
 
+  if (!json.animations) {
+    return []
+  }
+  const animations = json.animations
+  animations.forEach(m => {
+    m.channels
+  })
 
 
   return []
